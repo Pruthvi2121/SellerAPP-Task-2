22ce6236d700420edce2885609334b02e90c63b3
Pruthviraj <91754763+Pruthvi2121@users.noreply.github.com>
2023-10-04 23:18:42 +0530

    Navigation Setup

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,9 +5,15 @@ import { CgSandClock} from "react-icons/cg";
 import Data from './Data.json'
 import { Link } from 'react-router-dom';
 import { tab } from '@testing-library/user-event/dist/tab';
+import { useLocation, useNavigate } from "react-router-dom";
 export const Home = ()=> {
+    const navigate = useNavigate()
     const [tag, setTag] = useState("Mumbai")
+    const [cardCount, setCardCount] = useState(3)
     
+    const ShowMore = ()=>{
+      setCardCount(prev => prev + 3)
+    }
   return (
     <div className='mt-4 min-h-screen w-full md:w-11/12 xl:w-9/12 mx-auto '>
       <div className='w-1/2 mx-auto  text-gray-800 font-bold'>
@@ -19,7 +25,12 @@ export const Home = ()=> {
       
       {
         Data.tags.map((item)=>(
-         <button key={item} className={tag===item?"bg-[#3639E3] text-white px-6 py-2 rounded-full font-semibold ":"bg-[#EBEBFB] px-6 py-2 rounded-full font-semibold text-gray-600" } onClick={()=>setTag(item)}>{item}</button>
+         <button key={item} className={tag===item?"bg-[#3639E3] text-white px-6 py-2 rounded-full font-semibold ":"bg-[#EBEBFB] px-6 py-2 rounded-full font-semibold text-gray-600" }  
+          onClick={() => {
+          setTag(item);
+        setCardCount(3);
+        }}  >
+          {item}</button>
         ))
       }
       
@@ -27,7 +38,7 @@ export const Home = ()=> {
       <button className='border-[#3639E3] border px-6 py-2 ml-auto mr-2 rounded-full font-bold text-[#3639E3] flex item-center justify-center gap-2'>View All  <BsArrowRight className='my-auto text-lg'/> </button>
     </div>
 
-    <div className='grid grid-cols-3  gap-4 mt-4 mx-auto w-full '>
+    <div className='grid lg:grid-cols-3 md:grid-cols-2  gap-4 mt-4 mx-auto w-full '>
 
         {
             // Data.properties.map((item,i)=>(
@@ -37,17 +48,19 @@ export const Home = ()=> {
 
             Data.properties.filter((item)=>{
               return item.tag.toLowerCase().includes(tag.toLowerCase())
-            })
+            }).slice(0,cardCount)
             .map((item, i)=>(
-              <Card data={item} key={i}/>
+                <div key={i} onClick={()=>{navigate(`property/${item.id}`, { state:{item} })}}>
+                <Card data={item} />
+                </div>
             )) 
         }
           
     </div>
     <div className='mx-auto w-full flex my-4'>
-      <button className='bg-[#3639E3] px-6 py-2 rounded-full font-semibold text-white mx-auto flex items-center gap-2  hover:bg-[#3639e3af] duration-300 group'><CgSandClock className='text-lg duration-300 group-hover:animate-bounce'/>  Show More</button>
+      <button className='bg-[#3639E3] px-6 py-2 rounded-full font-semibold text-white mx-auto flex items-center gap-2  hover:bg-[#3639e3af] duration-300 group' onClick={ShowMore}><CgSandClock className='text-lg duration-300 group-hover:animate-bounce'/>  Show More</button>
     </div>
-   
+        
     
     </div>
   );
--- src/Components/Property.js
+++ src/Components/Property.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { Card } from "./Card";
+import { useLocation } from "react-router-dom";
 
-export const Property = ()=>{
-    const { id } = useParams();
-    return <>
-    <div className="flex h-screen justify-center items-center">
-     <h1 className="text-black">Propterty Page ..{id}</h1>
-     
-    </div>
+export const Property = () => {
+  const state = useLocation();
+  const { item } = state.state;
+  const { id } = useParams();
 
+  return (
+    <>
+      <div className="flex flex-col h-screen justify-center items-center">
+        <h1 className="text-black">Property Page ..{id}</h1>
+        
+        <Card data={item} />
+      </div>
     </>
-}
+  );
+};
